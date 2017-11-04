24e83e2b78ab49eb91140826d6f55eb3b8a9e507
Sudheer <singampallisudheer@gmail.com>
2017-11-04 13:12:51 +0900

    base code migrated

---
+++ Script/Script.js
@@ -0,0 +1,29 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function myMap() {
+var location = {lat:35.625101 , lng:139.720581};
+var map = new google.maps.Map(document.getElementById("map"), {
+	zoom : 15,
+	center : location
+	});
+var marker = new google.maps.Marker({
+	position : location,
+	map : map
+});
+    }
+
+            
